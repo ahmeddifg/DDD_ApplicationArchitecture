8e84093a3a1c750ad4a217e0b9a352b9d079dcf0
ahmeddif <ahmed.def.g@gmail.com>
2022-10-29 16:59:21 +0300

    adding domain events interfaces

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderPaidEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderPaidEvent.java
@@ -5,8 +5,8 @@ import com.food.ordering.system.order.service.domain.entity.Order;
 
 import java.time.ZonedDateTime;
 
-public class OrderPayEvent  extends OrderEvent {
-    public OrderPayEvent(Order order, ZonedDateTime zonedDateTime) {
+public class OrderPaidEvent extends OrderEvent {
+    public OrderPaidEvent(Order order, ZonedDateTime zonedDateTime) {
         super(order, zonedDateTime);
     }
 }
